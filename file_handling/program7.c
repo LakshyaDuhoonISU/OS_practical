5ae29c922e8dd5c2633c6c2deace66a561945806
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2024-05-29 20:02:17 +0530

    Update program7.c

--- file_handling/program7.c
+++ file_handling/program7.c
@@ -9,21 +9,19 @@ int main() {
     char buffer[BUFFER_SIZE];
     ssize_t bytesRead;
 
-    // Open the file for reading
+    
     file = open("example.txt", O_RDONLY);
     if (file == -1) {
         perror("Error opening file");
         return 1;
     }
 
-    // Read and print each line of the file
+    
     while ((bytesRead = read(file, buffer, BUFFER_SIZE)) > 0) {
         for (int i = 0; i < bytesRead; i++) {
             if (buffer[i] == '\n') {
-                // Print line
-                buffer[i] = '\0'; // Null-terminate the string
+                buffer[i] = '\0';
                 printf("%s\n", buffer);
-                // Shift buffer content
                 int remainingBytes = bytesRead - (i + 1);
                 if (remainingBytes > 0) {
                     for (int j = 0; j < remainingBytes; j++) {
@@ -32,21 +30,20 @@ int main() {
                 }
                 break;
             } else if (i == bytesRead - 1) {
-                // Print remaining buffer content as a line
-                buffer[i + 1] = '\0'; // Null-terminate the string
+                buffer[i + 1] = '\0';
                 printf("%s", buffer);
             }
         }
     }
 
-    // Check for read errors
+    
     if (bytesRead == -1) {
         perror("Error reading file");
         close(file);
         return 1;
     }
 
-    // Close the file
+
     close(file);
 
     return 0;
