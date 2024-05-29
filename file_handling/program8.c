ddfd18cdc95782be0f911843c9be12067835f65b
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2024-05-29 20:02:45 +0530

    Update program8.c

--- file_handling/program8.c
+++ file_handling/program8.c
@@ -9,14 +9,14 @@ int main() {
     char buffer[BUFFER_SIZE];
     ssize_t bytesRead;
 
-    // Open the source file for reading
+
     sourceFile = open("dummy.txt", O_RDONLY);
     if (sourceFile == -1) {
         perror("Error opening source file");
         return 1;
     }
 
-    // Open the destination file for writing
+    
     destinationFile = open("example.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (destinationFile == -1) {
         perror("Error opening destination file");
@@ -24,7 +24,7 @@ int main() {
         return 1;
     }
 
-    // Copy content from source file to destination file
+    
     while ((bytesRead = read(sourceFile, buffer, BUFFER_SIZE)) > 0) {
         if (write(destinationFile, buffer, bytesRead) != bytesRead) {
             perror("Error writing to destination file");
@@ -34,7 +34,7 @@ int main() {
         }
     }
 
-    // Check for read errors
+    
     if (bytesRead == -1) {
         perror("Error reading source file");
         close(sourceFile);
@@ -42,7 +42,7 @@ int main() {
         return 1;
     }
 
-    // Close both files
+    
     close(sourceFile);
     close(destinationFile);
 
