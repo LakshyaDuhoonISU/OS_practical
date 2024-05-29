8bd5a18cf44003666291836be8bdd3c7c6f3e41b
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2024-05-29 20:06:00 +0530

    Update program9.sh

--- file_perms/program9.sh
+++ file_perms/program9.sh
@@ -3,11 +3,10 @@
 # Script to rename all .txt files to .bak in the current directory
 
 for txt_file in *.txt; do
-  # Check if there are any .txt files to process
   if [[ -e "$txt_file" ]]; then
-    # Construct the new filename with .bak extension
+    # Constructing the new filename with .bak extension
     bak_file="${txt_file%.txt}.bak"
-    # Rename the file
+    # Renaming the file
     mv -v "$txt_file" "$bak_file"
   else
     echo "No .txt files found in the current directory."
