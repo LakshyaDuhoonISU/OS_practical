74b9b750bf3c6e569b6b11cfc8c6b3cbc684309f
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2024-05-29 20:03:07 +0530

    Update program9.c

--- file_handling/program9.c
+++ file_handling/program9.c
@@ -10,14 +10,14 @@ int main() {
     ssize_t bytesRead;
     int lineCount = 0;
 
-    // Open the file for reading
+    
     file = open("example.txt", O_RDONLY);
     if (file == -1) {
         perror("Error opening file");
         return 1;
     }
 
-    // Count the number of lines in the file
+    
     while ((bytesRead = read(file, buffer, BUFFER_SIZE)) > 0) {
         for (int i = 0; i < bytesRead; i++) {
             if (buffer[i] == '\n') {
@@ -26,17 +26,17 @@ int main() {
         }
     }
 
-    // Check for read errors
+    
     if (bytesRead == -1) {
         perror("Error reading file");
         close(file);
         return 1;
     }
 
-    // Close the file
+    
     close(file);
 
-    // Increment line count for the last line if it doesn't end with a newline
+    
     if (bytesRead > 0 && buffer[bytesRead - 1] != '\n') {
         lineCount++;
     }
