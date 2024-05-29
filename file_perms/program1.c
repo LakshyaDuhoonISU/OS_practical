365000a8c2d16c12238beebf2a83ebde14bd5e2c
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2024-05-29 20:03:55 +0530

    Update program1.c

--- file_perms/program1.c
+++ file_perms/program1.c
@@ -3,13 +3,10 @@
 #include <sys/stat.h>
 
 int main() {
-    // Path to the file whose permissions will be changed
     const char *file_path = "dummy.txt";
 
-    // New file permissions (in octal)
-    mode_t new_permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; // Read and write for owner, read for group and others
+    mode_t new_permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
 
-    // Change file permissions using chmod system call
     if (chmod(file_path, new_permissions) == -1) {
         perror("chmod");
         exit(EXIT_FAILURE);
