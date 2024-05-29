e9099a4791216d985a3fbdf5aa46725017851090
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2024-05-29 19:59:29 +0530

    Update program10.c

--- file_handling/program10.c
+++ file_handling/program10.c
@@ -3,17 +3,17 @@
 #include <fcntl.h>
 #include <string.h>
 int main() {
-    // File path to check
+    
     char filename[100];
     scanf("%s", filename);
 
-    // Attempt to open the file with read-only mode
+    
     int fd = open(filename, O_RDONLY);
 
-    // Check if file exists
+    
     if (fd != -1) {
         printf("File exists.\n");
-        // Close the file descriptor
+        
         close(fd);
     } else {
         printf("File does not exist or cannot be opened.\n");
