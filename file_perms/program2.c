793f816a028f67cdff138f8872beb9c1e4638043
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2024-05-29 20:05:07 +0530

    Update program2.c

--- file_perms/program2.c
+++ file_perms/program2.c
@@ -3,14 +3,10 @@
 #include <unistd.h>
 
 int main() {
-    // Path to the file whose owner will be changed
     const char *file_path = "dummy.txt";
+    uid_t new_owner_uid = 1001; 
+    gid_t new_owner_gid = 1001;
 
-    // New owner user ID and group ID (you may need to change these values)
-    uid_t new_owner_uid = 1001; // Replace with the desired user ID
-    gid_t new_owner_gid = 1001; // Replace with the desired group ID
-
-    // Change file owner using chown system call
     if (chown(file_path, new_owner_uid, new_owner_gid) == -1) {
         perror("chown");
         exit(EXIT_FAILURE);
